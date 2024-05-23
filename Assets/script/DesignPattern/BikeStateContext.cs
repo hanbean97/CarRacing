3b86443c5f8033da103a902ff58c3eecec6e5cdc
hanbean97 <coldbean97@gmail.com>
2024-05-23 12:24:52 +0900

    sad

---
+++ Assets/script/DesignPattern/BikeStateContext.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+interface IBikeState
+{
+    void Handle();
+}
+public class BikeStateContext : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
